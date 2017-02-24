ec3e3030825c76d78deb329c57a945722b3bdba5
Duncan Doyle <Duncan.Doyle@gmail.com>
2017-02-24 15:40:55 +0100

    Initial implementation of the jBPM Task Query extension. Basic client implementation works. Extension boots and configures. Re-uses jBPM extension's QueryService.

--- kie-server-parent/kie-server-api/src/main/java/org/kie/server/api/KieServerConstants.java
+++ kie-server-parent/kie-server-api/src/main/java/org/kie/server/api/KieServerConstants.java
@@ -29,6 +29,7 @@ public class KieServerConstants {
     public static final String KIE_JBPM_UI_SERVER_EXT_DISABLED = "org.jbpm.ui.server.ext.disabled";
     public static final String KIE_OPTAPLANNER_SERVER_EXT_DISABLED = "org.optaplanner.server.ext.disabled";
     public static final String KIE_CASE_SERVER_EXT_DISABLED = "org.jbpm.case.server.ext.disabled";
+    public static final String KIE_JBM_TASKQUERIES_SERVER_EXT_DISABLED = "org.jbpm.taskqueries.server.ext.disabled";
 
     public static final String KIE_DROOLS_FILTER_REMOTEABLE_CLASSES = "org.drools.server.filter.classes";
 
@@ -99,6 +100,7 @@ public class KieServerConstants {
     public static final String CAPABILITY_BPM_UI = "BPM-UI"; // Business Process Management UI
     public static final String CAPABILITY_BRP = "BRP"; // Business Resource Planning
     public static final String CAPABILITY_CASE = "CaseMgmt"; // Case Management
+    public static final String CAPABILITY_BPM_TASK_QUERIES = "BPMTaskQueries"; // BPM Task Queries
     
     public static final String FAILURE_REASON_PROP = "failure-reason";
 
